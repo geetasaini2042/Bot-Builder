071bb7f54b852859e374bfea0543627d3589c094
geetasaini2042 <Geetasaini2042@gmail.com>
2025-05-03 21:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,10 +113,12 @@ def handle_document(bot_token,full_update, message):
     client.send_message(bot_token, chat_id,yioip )
     if "status" in yioip:
             status = yioip.get("status")
+            client.send_message(bot_token, chat_id,f"status v {status}" )
             if status == "":
               print("Nothing in status")
             else:
               if status == "awaiting_file_upload":
+                client.send_message(bot_token, chat_id, f"Calling Bot server.!")
                 BotServer.add_file(bot_token, full_update)
     client.send_message(bot_token, chat_id, f"Received document: {file_name}\nfile_id: {file_id}")
 
